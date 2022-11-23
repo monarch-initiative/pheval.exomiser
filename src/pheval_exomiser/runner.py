d5118e8a360182b8f3b6b6c96a03ec369edf4b56
Vinicius de Souza <souzadevinicius@gmail.com>
2022-11-23 14:40:04 -0300

    testing pheval cli using exomiser as runner

---
+++ src/pheval_exomiser/runner.py
@@ -0,0 +1,27 @@
+"""Exomiser Runner"""
+from dataclasses import dataclass
+import click
+
+from pheval.pipeline import PhEvalRunner
+
+
+@dataclass
+class ExomiserPhEvalRunner(PhEvalRunner):
+    """_summary_ """
+    inputdir: click.Path
+    testdatadir: click.Path
+    tmpdir: click.Path
+    outputdir: click.Path
+    config: click.Path
+
+    def prepare(self):
+        """prepare"""
+        print("preparing")
+
+    def run(self):
+        """run"""
+        print("running with exomiser")
+
+    def post_process(self):
+        """post_process """
+        print("post processing")
