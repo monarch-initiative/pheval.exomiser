44db3588e0ea63045b9fc3fef9142e90ff312029
Yasemin Bridges <y.bridges@qmul.ac.uk>
2022-12-21 21:52:03 +0000

    adding docstrings

--- src/pheval_exomiser/prepare/yaml_to_family_phenopacket.py
+++ src/pheval_exomiser/prepare/yaml_to_family_phenopacket.py
@@ -33,16 +33,19 @@ from ..utils.phenopacket_utils import write_phenopacket
 
 
 def load_ontology():
+    """Loads human phenotype ontology."""
     resource = OntologyResource(slug="hp.obo", local=False)
     return ProntoImplementation(resource)
 
 
 def load_genotype_ontology():
+    """Loads genotype ontology"""
     genotype_resource = OntologyResource(slug="geno.owl", local=False)
     return ProntoImplementation(genotype_resource)
 
 
 def exomiser_analysis_yml_reader(yaml_job_file_path: Path) -> dict:
+    """Reads an exomiser analysis yaml file."""
     with open(yaml_job_file_path) as yaml_job_file:
         yaml_job = yaml.safe_load(yaml_job_file)
     yaml_job_file.close()
@@ -50,10 +53,12 @@ def exomiser_analysis_yml_reader(yaml_job_file_path: Path) -> dict:
 
 
 def read_diagnoses_file(diagnoses_file_path: Path) -> pd.DataFrame:
+    """Reads a diagnoses file."""
     return pd.read_csv(diagnoses_file_path, delimiter="t")
 
 
 def read_pedigree_file(pedigree_path: Path) -> list[str]:
+    """Returns the contents of a pedigree file"""
     return open(pedigree_path).readlines()
 
 
@@ -93,7 +98,8 @@ class ExomiserYamlToPhenopacketConverter:
         self.hgnc_data = hgnc_data
 
     @staticmethod
-    def construct_individual_message(yaml_job: dict, diagnoses: pd.DataFrame) -> Individual:
+    def construct_individual(yaml_job: dict, diagnoses: pd.DataFrame) -> Individual:
+        """Constructs individual for phenopacket."""
         return Individual(
             id=yaml_job["analysis"]["proband"],
             sex=diagnoses[diagnoses.ProbandId == yaml_job["analysis"]["proband"]]
@@ -103,9 +109,11 @@ class ExomiserYamlToPhenopacketConverter:
 
     @staticmethod
     def get_diagnoses_for_proband(yaml_job: dict, diagnoses: pd.DataFrame):
+        """Gets all diagnoses for proband."""
         return diagnoses.loc[diagnoses["ProbandId"] == yaml_job["analysis"]["proband"]]
 
     def construct_phenotypic_interpretations(self, yaml_job: dict) -> list[PhenotypicFeature]:
+        """Constructs the phenotypic features for the proband."""
         hpo_ids = yaml_job["analysis"]["hpoIds"]
         phenotypic_features = []
         for hpo_id in hpo_ids:
@@ -121,6 +129,7 @@ class ExomiserYamlToPhenopacketConverter:
 
     @staticmethod
     def construct_vcf_record(yaml_job: dict, diagnosis: pd.DataFrame) -> VcfRecord:
+        """Constructs the VCF record for a diagnosis."""
         return VcfRecord(
             genome_assembly=yaml_job["analysis"]["genomeAssembly"],
             chrom=diagnosis["Chr"],
@@ -130,12 +139,14 @@ class ExomiserYamlToPhenopacketConverter:
         )
 
     def construct_allelic_state(self, diagnosis: pd.DataFrame) -> OntologyClass:
+        """Constructs the allelic state for a diagnosis."""
         return OntologyClass(
             id=list(self.genotype_ontology.basic_search(diagnosis["Genotype"].lower()))[0],
             label=diagnosis["Genotype"].lower(),
         )
 
     def construct_gene_descriptor(self, diagnosis: pd.DataFrame) -> GeneDescriptor:
+        """Constructs the Gene Descriptor for a diagnosis."""
         try:
             return GeneDescriptor(
                 value_id=self.hgnc_data[diagnosis["Gene"]]["ensembl_id"],
@@ -153,6 +164,7 @@ class ExomiserYamlToPhenopacketConverter:
     def construct_variation_descriptor(
         self, yaml_job: dict, diagnosis: pd.DataFrame
     ) -> VariationDescriptor:
+        """Constructs a variation descriptor for a diagnosis."""
         return VariationDescriptor(
             id=yaml_job["analysis"]["proband"]
             + ":"
@@ -169,6 +181,7 @@ class ExomiserYamlToPhenopacketConverter:
     def construct_variant_interpretation(
         self, yaml_job: dict, diagnosis: pd.DataFrame
     ) -> VariantInterpretation:
+        """Constructs the variant interpretation for a diagnosis."""
         return VariantInterpretation(
             variation_descriptor=self.construct_variation_descriptor(yaml_job, diagnosis),
         )
@@ -176,6 +189,7 @@ class ExomiserYamlToPhenopacketConverter:
     def construct_genomic_interpretations(
         self, yaml_job: dict, diagnoses: pd.DataFrame
     ) -> list[GenomicInterpretation]:
+        """Constructs a list of genomic interpretations for a proband."""
         genomic_interpretations = []
         for _index, row in self.get_diagnoses_for_proband(yaml_job, diagnoses).iterrows():
             genomic_interpretation = GenomicInterpretation(
@@ -188,6 +202,7 @@ class ExomiserYamlToPhenopacketConverter:
         return genomic_interpretations
 
     def construct_diagnosis(self, yaml_job: dict, diagnoses: pd.DataFrame) -> Diagnosis:
+        """Constructs the diagnosis for a proband."""
         return Diagnosis(
             genomic_interpretations=self.construct_genomic_interpretations(yaml_job, diagnoses)
         )
@@ -195,6 +210,7 @@ class ExomiserYamlToPhenopacketConverter:
     def construct_interpretations(
         self, yaml_job: dict, diagnoses: pd.DataFrame
     ) -> list[Interpretation]:
+        """Constructs interpretations for a proband."""
         return [
             Interpretation(
                 id=yaml_job["analysis"]["proband"] + "-interpretation",
@@ -204,6 +220,7 @@ class ExomiserYamlToPhenopacketConverter:
 
     @staticmethod
     def construct_meta_data() -> MetaData:
+        """Constructs the meta data."""
         timestamp = Timestamp()
         timestamp.GetCurrentTime()
         return MetaData(
@@ -224,6 +241,7 @@ class ExomiserYamlToPhenopacketConverter:
 
     @staticmethod
     def construct_files(yaml_job_file: dict) -> list[File]:
+        """Constructs the files."""
         return [
             File(
                 uri=yaml_job_file["analysis"]["vcf"],
@@ -236,6 +254,7 @@ class ExomiserYamlToPhenopacketConverter:
 
 
 def construct_pedigree(pedigree: list[str]) -> tuple[str, Pedigree]:
+    """Constructs the pedigree message from a pedigree."""
     persons = []
     family_id = None
     for individual in pedigree:
@@ -289,9 +308,10 @@ def construct_phenopacket(
     diagnoses: pd.DataFrame,
     exomiser_yaml_to_phenopacket_converter: ExomiserYamlToPhenopacketConverter,
 ) -> Phenopacket:
+    """Constucts a phenopacket."""
     return Phenopacket(
         id=yaml_job_file["analysis"]["proband"],
-        subject=exomiser_yaml_to_phenopacket_converter.construct_individual_message(
+        subject=exomiser_yaml_to_phenopacket_converter.construct_individual(
             yaml_job=yaml_job_file, diagnoses=diagnoses
         ),
         phenotypic_features=exomiser_yaml_to_phenopacket_converter.construct_phenotypic_interpretations(
@@ -311,6 +331,7 @@ def construct_family(
     exomiser_yaml_to_phenopacket_converter: ExomiserYamlToPhenopacketConverter,
     pedigree: list[str],
 ) -> Family:
+    """Constructs a Family"""
     phenopacket = construct_phenopacket(
         yaml_job_file, diagnoses, exomiser_yaml_to_phenopacket_converter
     )
@@ -332,6 +353,7 @@ def create_phenopacket(
     diagnoses: pd.DataFrame,
     exomiser_converter: ExomiserYamlToPhenopacketConverter,
 ) -> Phenopacket or Family:
+    """Constructs either a family or phenopacket from an analysis yaml."""
     yaml_job = exomiser_analysis_yml_reader(yaml_job_file)
     phenopacket = (
         construct_phenopacket(yaml_job, diagnoses, exomiser_converter)
@@ -361,6 +383,8 @@ def create_phenopacket(
 def convert_exomiser_analysis_yamls_to_phenopacket(
     output_dir: Path, directory: Path, diagnoses_file: Path
 ):
+    """Converts an Exomiser YAML file to a phenopacket schema given a .tsv diagnoses file containing the following
+    required fields: ... """
     try:
         output_dir.mkdir()
     except FileExistsError:
@@ -391,6 +415,8 @@ def convert_exomiser_analysis_yamls_to_phenopacket(
 def convert_exomiser_analysis_yaml_to_phenopacket(
     output_dir: Path, yaml_file: Path, diagnoses_file: Path
 ):
+    """Converts Exomiser YAML files to the phenopacket schema given a .tsv diagnoses file containing the following
+    required fields: ... """
     try:
         output_dir.mkdir()
     except FileExistsError:
