2aa82dcbe8e2e3141a58bd7f17135d740a5b8443
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-11 20:46:26 +0000

    removing config parser method - retrieving config parameters from parser

--- src/pheval_exomiser/prepare/prepare.py
+++ src/pheval_exomiser/prepare/prepare.py
@@ -1,57 +1,42 @@
-import configparser
 from pathlib import Path
 
 from pheval.prepare.create_noisy_phenopackets import create_scrambled_phenopackets
-from pheval.prepare.create_spiked_vcf import create_spiked_vcfs_for_phenopackets
-from pheval.prepare.update_phenopacket import update_phenopackets_causative_genes
+from pheval.prepare.create_spiked_vcf import create_spiked_vcfs
+from pheval.prepare.update_phenopacket import update_phenopackets
+
+from pheval_exomiser.config_parser import ExomiserConfig
 
 
-def read_config(config_path: Path):
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    return config
+def prepare_updated_phenopackets(testdata_dir: Path, config: ExomiserConfig) -> None:
+    """Updates the gene data for phenopackets."""
+    if config.prepare.update_phenopacket_gene_identifier.update is True:
+        print("...updating phenopacket causative gene data...")
+        update_phenopackets(
+            phenopacket_dir=testdata_dir.joinpath("phenopackets"),
+            gene_identifier=config.prepare.update_phenopacket_gene_identifier.gene_identifer_to_update
+        )
 
 
-def prepare_updated_phenopackets(testdatadir: Path, config_path: Path):
-    config = read_config(config_path)
-    print("...updating phenopacket causative gene data...") if config["PREPARE"][
-        "UpdatePhenopacketGeneData"
-    ] == "yes" else print("")
-    update_phenopackets_causative_genes(
-        phenopacket_dir=testdatadir.joinpath("phenopackets"),
-        gene_identifier=config["PREPARE"]["GeneIdentifierToUpdate"],
-    ) if config["PREPARE"]["UpdatePhenopacketGeneData"] == "yes" else None
+def prepare_scrambled_phenopackets(testdata_dir: Path, config: ExomiserConfig):
+    """Scrambles the phenopacket phenotypic profiles."""
+    if config.prepare.scramble.scramble_phenopacket != 0:
+        print("...scrambling phenopacket phenotypic profiles...")
+        create_scrambled_phenopackets(output_dir=testdata_dir.joinpath(
+            f"scrambled_phenopackets_{config.prepare.scramble.scramble_phenopacket}"
+        ),
+            output_file_suffix=f"scrambled_{config.prepare.scramble.scramble_phenopacket}",
+            phenopacket_dir=testdata_dir.joinpath("phenopackets"),
+            scramble_factor=config.prepare.scramble.scramble_phenopacket,
+        )
 
 
-def prepare_scrambled_phenopackets(testdatadir: Path, config_path: Path):
-    config = read_config(config_path)
-    print("...scrambling phenopackets...") if config["PREPARE"][
-        "ScramblePhenopacket"
-    ] == "yes" else print("")
-    create_scrambled_phenopackets(
-        output_dir=testdatadir.joinpath("scrambled_phenopackets"),
-        output_file_suffix="scrambled",
-        phenopacket_dir=testdatadir.joinpath("phenopackets"),
-        scramble_factor=float(config["PREPARE"]["ScramblePhenopacketScrambleFactor"]),
-    ) if config["PREPARE"]["ScramblePhenopacket"] == "yes" else None
-
-
-def prepare_spiked_vcfs(testdatadir: Path, config_path: Path):
-    config = read_config(config_path)
-    print("...spiking vcfs...") if config["PREPARE"]["CreateSpikedVcf"] == "yes" else print("")
-    vcf_dir = (
-        None
-        if config["PREPARE"]["PathToTemplateVcfDirectory"] == ""
-        else Path(config["PREPARE"]["PathToTemplateVcfDirectory"])
-    )
-    template_vcf = (
-        None
-        if config["PREPARE"]["PathToTemplateVcf"] == ""
-        else Path(config["PREPARE"]["PathToTemplateVcf"])
-    )
-    create_spiked_vcfs_for_phenopackets(
-        output_dir=testdatadir.joinpath("vcfs"),
-        phenopacket_dir=testdatadir.joinpath("phenopackets"),
-        template_vcf_path=template_vcf,
-        vcf_dir=vcf_dir,
-    ) if config["PREPARE"]["CreateSpikedVcf"] == "yes" else None
+def prepare_spiked_vcfs(testdata_dir: Path, config: ExomiserConfig):
+    """Creates spiked vcf files with proband variants."""
+    if config.prepare.create_spiked_vcf.spike is True:
+        print("...spiking vcfs...")
+        create_spiked_vcfs(
+            output_dir=testdata_dir.joinpath("vcfs"),
+            phenopacket_dir=testdata_dir.joinpath("phenopackets"),
+            template_vcf_path=config.prepare.create_spiked_vcf.path_to_template_vcf,
+            vcf_dir=config.prepare.create_spiked_vcf.path_to_template_vcf_directory,
+        )
