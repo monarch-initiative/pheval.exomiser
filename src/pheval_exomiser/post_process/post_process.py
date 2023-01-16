700aceffabc76db5f38f43a9057852cf4b5b549e
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-16 16:49:14 +0000

    reformatting

--- src/pheval_exomiser/post_process/post_process.py
+++ src/pheval_exomiser/post_process/post_process.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from pheval_exomiser.config_parser import ExomiserConfig
 from pheval_exomiser.post_process.assess_prioritisation import (
+    CorrespondingExomiserInput,
     benchmark_directories_for_pairwise_comparison,
-    benchmark_directory, CorrespondingExomiserInput, benchmark_several_directories,
+    benchmark_directory,
+    benchmark_several_directories,
 )
 
 
@@ -22,36 +24,52 @@ def post_process_exomiser_results(output_dir: Path, config: ExomiserConfig):
         benchmark_directory(
             results_dir_and_input=CorrespondingExomiserInput(
                 phenopacket_dir=config.run.runs[0].path_to_input_phenopacket_data,
-                results_dir=Path(output_dir).joinpath(config.run.runs[0].run_identifier + "_results")),
+                results_dir=Path(output_dir).joinpath(
+                    config.run.runs[0].run_identifier + "_results"
+                ),
+            ),
             ranking_method=config.post_processing.ranking_method,
             output_prefix=config.post_processing.output_prefix,
             threshold=config.post_processing.threshold,
             gene_analysis=config.post_processing.benchmark_gene_prioritisation,
-            variant_analysis=config.post_processing.benchmark_variant_prioritisation
-
+            variant_analysis=config.post_processing.benchmark_variant_prioritisation,
         )
     elif len(config.run.runs) == 2:
-        benchmark_directories_for_pairwise_comparison(results_directories=[
-            CorrespondingExomiserInput(phenopacket_dir=config.run.runs[0].path_to_input_phenopacket_data,
-                                       results_dir=Path(output_dir).joinpath(
-                                           config.run.runs[0].run_identifier + "_results")),
-            CorrespondingExomiserInput(phenopacket_dir=config.run.runs[1].path_to_input_phenopacket_data,
-                                       results_dir=Path(output_dir).joinpath(
-                                           config.run.runs[1].run_identifier + "_results"))],
+        benchmark_directories_for_pairwise_comparison(
+            results_directories=[
+                CorrespondingExomiserInput(
+                    phenopacket_dir=config.run.runs[0].path_to_input_phenopacket_data,
+                    results_dir=Path(output_dir).joinpath(
+                        config.run.runs[0].run_identifier + "_results"
+                    ),
+                ),
+                CorrespondingExomiserInput(
+                    phenopacket_dir=config.run.runs[1].path_to_input_phenopacket_data,
+                    results_dir=Path(output_dir).joinpath(
+                        config.run.runs[1].run_identifier + "_results"
+                    ),
+                ),
+            ],
             ranking_method=config.post_processing.ranking_method,
             output_prefix=config.post_processing.output_prefix,
             threshold=config.post_processing.threshold,
             gene_analysis=config.post_processing.benchmark_gene_prioritisation,
-            variant_analysis=config.post_processing.benchmark_variant_prioritisation
+            variant_analysis=config.post_processing.benchmark_variant_prioritisation,
         )
     else:
-        runs = [CorrespondingExomiserInput(phenopacket_dir=run.path_to_input_phenopacket_data,
-                                           results_dir=Path(output_dir).joinpath(run.run_identifier + "_results")) for
-                run in config.run.runs]
-        benchmark_several_directories(results_directories=runs, ranking_method=config.post_processing.ranking_method,
-                                      output_prefix=config.post_processing.output_prefix,
-                                      threshold=config.post_processing.threshold,
-                                      gene_analysis=config.post_processing.benchmark_gene_prioritisation,
-                                      variant_analysis=config.post_processing.benchmark_variant_prioritisation
-                                      )
+        runs = [
+            CorrespondingExomiserInput(
+                phenopacket_dir=run.path_to_input_phenopacket_data,
+                results_dir=Path(output_dir).joinpath(run.run_identifier + "_results"),
+            )
+            for run in config.run.runs
+        ]
+        benchmark_several_directories(
+            results_directories=runs,
+            ranking_method=config.post_processing.ranking_method,
+            output_prefix=config.post_processing.output_prefix,
+            threshold=config.post_processing.threshold,
+            gene_analysis=config.post_processing.benchmark_gene_prioritisation,
+            variant_analysis=config.post_processing.benchmark_variant_prioritisation,
+        )
     print("done")
