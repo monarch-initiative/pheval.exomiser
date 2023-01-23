1dccc65078d0458c5b58f3697ecce87bf96da5e1
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-23 16:58:57 +0000

    restructuring runner

--- src/pheval_exomiser/runner.py
+++ src/pheval_exomiser/runner.py
@@ -5,12 +5,7 @@ from pathlib import Path
 from pheval.runners.runner import PhEvalRunner
 
 from pheval_exomiser.config_parser import parse_exomiser_config
-from pheval_exomiser.post_process.post_process import post_process_exomiser_results
-from pheval_exomiser.prepare.prepare import (
-    prepare_scrambled_phenopackets,
-    prepare_spiked_vcfs,
-    prepare_updated_phenopackets,
-)
+from pheval_exomiser.post_process.post_process import post_process_result_format
 from pheval_exomiser.run.run import prepare_batch_files, run_exomiser
 
 
@@ -27,34 +22,42 @@ class ExomiserPhEvalRunner(PhEvalRunner):
     def prepare(self):
         """prepare"""
         print("preparing")
-        config = parse_exomiser_config(self.config_file)
-        try:
-            Path(self.input_dir).mkdir()
-        except FileExistsError:
-            pass
-        prepare_updated_phenopackets(
-            input_dir=Path(self.input_dir).joinpath("phenopackets"),
-            testdata_dir=Path(self.testdata_dir),
-            config=config,
-        )
-        prepare_scrambled_phenopackets(
-            input_dir=Path(self.input_dir), testdata_dir=Path(self.testdata_dir), config=config
-        )
-        prepare_spiked_vcfs(
-            input_dir=Path(self.input_dir), testdata_dir=Path(self.testdata_dir), config=config
-        )
+        # config = parse_exomiser_config(self.config_file)
+        # try:
+        #     Path(self.input_dir).mkdir()
+        # except FileExistsError:
+        #     pass
+        # prepare_updated_phenopackets(
+        #     input_dir=Path(self.input_dir).joinpath("phenopackets"),
+        #     testdata_dir=Path(self.testdata_dir),
+        #     config=config,
+        # )
+        # prepare_scrambled_phenopackets(
+        #     input_dir=Path(self.input_dir), testdata_dir=Path(self.testdata_dir), config=config
+        # )
+        # prepare_spiked_vcfs(
+        #     input_dir=Path(self.input_dir), testdata_dir=Path(self.testdata_dir), config=config
+        # )
 
     def run(self):
         """run"""
         print("running with exomiser")
         config = parse_exomiser_config(self.config_file)
         prepare_batch_files(
-            input_dir=Path(self.input_dir), output_dir=Path(self.output_dir), config=config
+            output_dir=Path(self.output_dir), config=config, testdata_dir=self.testdata_dir
         )
-        run_exomiser(output_dir=self.output_dir, config=config)
+        run_exomiser(
+            input_dir=self.input_dir,
+            testdata_dir=self.testdata_dir,
+            output_dir=self.output_dir,
+            config=config,
+        )
 
     def post_process(self):
         """post_process"""
         print("post processing")
         config = parse_exomiser_config(self.config_file)
-        post_process_exomiser_results(output_dir=self.output_dir, config=config)
+        post_process_result_format(
+            output_dir=Path(self.output_dir).joinpath(f"{config.run.run_identifier}_results"),
+            config=config,
+        )
