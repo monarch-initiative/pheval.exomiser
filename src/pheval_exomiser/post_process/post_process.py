5d40b7b3d20a13527ef10622f0f8689e00bd5f00
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-24 16:59:52 +0000

    using f strings

--- src/pheval_exomiser/post_process/post_process.py
+++ src/pheval_exomiser/post_process/post_process.py
@@ -5,14 +5,15 @@ from pheval_exomiser.config_parser import ExomiserConfig
 from pheval_exomiser.post_process.post_process_results_format import create_standardised_results
 
 
-def post_process_result_format(input_dir: Path, testdata_dir: Path, output_dir: Path, config: ExomiserConfig):
+def post_process_result_format(
+    input_dir: Path, testdata_dir: Path, output_dir: Path, config: ExomiserConfig
+):
     """Standardise Exomiser json format to separated gene and variant results."""
     print("...standardising results format...")
     run_output_dir = Path(output_dir).joinpath(
-        "exomiser" + "_" + config.run.exomiser_configurations.exomiser_version.replace(".",
-                                                                                       "_") + "_" + os.path.basename(
-            input_dir) + "/" + os.path.basename(
-            testdata_dir) + "_results")
+        f"exomiser_{config.run.exomiser_configurations.exomiser_version.replace('.', '_')}"
+        f"_{os.path.basename(input_dir)}{os.sep}{os.path.basename(testdata_dir)}_results"
+    )
     create_standardised_results(
         results_dir=Path(run_output_dir).joinpath("exomiser_results"),
         output_dir=run_output_dir,
