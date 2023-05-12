c184edec73c965c7fdb154a0b1cd9832e476e226
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-12 15:30:35 +0100

    removing extra parameters

--- src/pheval_exomiser/prepare/tool_specific_configuration_options.py
+++ src/pheval_exomiser/prepare/tool_specific_configuration_options.py
@@ -10,14 +10,8 @@ class ApplicationProperties(BaseModel):
         remm_version (str): Version of the REMM database
         cadd_version (str): Version of the CADD database
         hg19_data_version (str): Data version of the hg19 Exomiser data
-        hg19_cadd_snv_path (Path): The file name of the CADD hg19 SNV data file
-        hg19_cadd_indel_path (Path): The file name of the CADD hg19 indel data file
-        hg19_remm_path (bool): Boolean to include hg19 remm path or not
         hg19_local_frequency_path (Path): The file name of the hg19 local frequency file
         hg38_data_version (str): Data version of the hg38 Exomiser data
-        hg38_cadd_snv_path (Path): The file name of the CADD hg38 SNV data file
-        hg38_cadd_indel_path (Path): The file name of the CADD hg38 indel data file
-        hg38_remm_path (bool): Boolean to include hg38 remm path or not
         hg38_local_frequency_path (Path): The file name of the hg38 local frequency file
         phenotype_data_version (str): Data version of the Exomiser phenotype data
         cache_caffeine_spec (int): Cache limit
@@ -26,14 +20,8 @@ class ApplicationProperties(BaseModel):
     remm_version: str = Field(None)
     cadd_version: str = Field(None)
     hg19_data_version: str = Field(None)
-    hg19_cadd_snv_path: Path = Field(None)
-    hg19_cadd_indel_path: Path = Field(None)
-    hg19_remm_path: bool = Field(None)
     hg19_local_frequency_path: Path = Field(None)
     hg38_data_version: str = Field(None)
-    hg38_cadd_snv_path: Path = Field(None)
-    hg38_cadd_indel_path: Path = Field(None)
-    hg38_remm_path: bool = Field(None)
     hg38_local_frequency_path: Path = Field(None)
     phenotype_data_version: str = Field(None)
     cache_caffeine_spec: int = Field(None)
