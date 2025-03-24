24ee96cd746fe773fafe23f257e4c0b518525ccf
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-03-24 11:39:48 +0000

    use polars for extracting results

--- src/pheval_exomiser/post_process/post_process_results_format.py
+++ src/pheval_exomiser/post_process/post_process_results_format.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python
-import json
+import uuid
+from enum import Enum
 from pathlib import Path
 
 import click
+import polars as pl
 from pheval.post_processing.post_processing import (
-    PhEvalDiseaseResult,
-    PhEvalGeneResult,
-    PhEvalVariantResult,
-    generate_pheval_result,
+    SortOrder,
+    generate_disease_result,
+    generate_gene_result,
+    generate_variant_result,
 )
 from pheval.utils.file_utils import files_with_suffix
 
 
-def read_exomiser_json_result(exomiser_result_path: Path) -> dict:
-    """Load Exomiser json result."""
-    with open(exomiser_result_path) as exomiser_json_result:
-        exomiser_result = json.load(exomiser_json_result)
-    exomiser_json_result.close()
-    return exomiser_result
+class ModeOfInheritance(Enum):
+    AUTOSOMAL_DOMINANT = 1
+    AUTOSOMAL_RECESSIVE = 2
+    X_DOMINANT = 1
+    X_RECESSIVE = 2
+    MITOCHONDRIAL = 3
 
 
 def trim_exomiser_result_filename(exomiser_result_path: Path) -> Path:
@@ -25,213 +26,144 @@ def trim_exomiser_result_filename(exomiser_result_path: Path) -> Path:
     return Path(str(exomiser_result_path.name).replace("-exomiser", ""))
 
 
-class PhEvalGeneResultFromExomiserJsonCreator:
-    def __init__(self, exomiser_json_result: [dict], score_name: str):
-        self.exomiser_json_result = exomiser_json_result
-        self.score_name = score_name
+def extract_gene_results_from_json(
+    exomiser_json_result: pl.DataFrame, score_name: str
+) -> pl.DataFrame:
+    return exomiser_json_result.select(
+        [
+            pl.col("geneSymbol").alias("gene_symbol"),
+            pl.col("geneIdentifier").struct.field("geneId").alias("gene_identifier"),
+            pl.col(score_name).fill_null(0).round(4).alias("score"),
+        ]
+    ).drop_nulls()
 
-    @staticmethod
-    def _find_gene_symbol(result_entry: dict) -> str:
-        """Return gene symbol from Exomiser result entry."""
-        return result_entry["geneSymbol"]
 
-    @staticmethod
-    def _find_gene_identifier(result_entry: dict) -> str:
-        """Return ensembl gene identifier from Exomiser result entry."""
-        return result_entry["geneIdentifier"]["geneId"]
+def extract_disease_results_from_json(exomiser_json_result: pl.DataFrame) -> pl.DataFrame:
+    return (
+        exomiser_json_result.select(
+            [
+                pl.col("priorityResults")
+                .struct.field("HIPHIVE_PRIORITY")
+                .struct.field("diseaseMatches")
+            ]
+        )
+        .explode("diseaseMatches")
+        .unnest("diseaseMatches")
+        .unnest("model")
+        .select([pl.col("diseaseId").alias("disease_identifier"), pl.col("score").round(4)])
+        .drop_nulls()
+    )
 
-    def _find_relevant_score(self, result_entry: dict):
-        """Return score from Exomiser result entry."""
-        return round(result_entry[self.score_name], 4)
 
-    def extract_pheval_gene_requirements(self) -> [PhEvalGeneResult]:
-        """Extract data required to produce PhEval gene output."""
-        simplified_exomiser_result = []
-        for result_entry in self.exomiser_json_result:
-            if self.score_name in result_entry:
-                simplified_exomiser_result.append(
-                    PhEvalGeneResult(
-                        gene_symbol=self._find_gene_symbol(result_entry),
-                        gene_identifier=self._find_gene_identifier(result_entry),
-                        score=self._find_relevant_score(result_entry),
+def extract_variant_results_from_json(
+    exomiser_json_result: pl.DataFrame, score_name: str
+) -> pl.DataFrame:
+    return (
+        exomiser_json_result.filter(pl.col("geneScores").is_not_null())
+        .select([pl.col("geneScores"), pl.col(score_name).alias("score"), pl.col("geneSymbol")])
+        .explode("geneScores")
+        .unnest("geneScores")
+        .filter(pl.col("contributingVariants").is_not_null())
+        .explode("contributingVariants")
+        .with_columns(
+            [
+                pl.col("contributingVariants").struct.field("contigName").alias("chrom"),
+                pl.col("contributingVariants").struct.field("start"),
+                pl.col("contributingVariants").struct.field("end"),
+                pl.col("contributingVariants").struct.field("ref"),
+                pl.col("contributingVariants")
+                .struct.field("alt")
+                .fill_null("")
+                .str.strip_chars("<>")
+                .alias("alt"),
+                pl.col("modeOfInheritance")
+                .map_elements(lambda moi: ModeOfInheritance[moi].value, return_dtype=pl.Int8)
+                .alias("moi_enum"),
+            ]
+        )
+        .with_columns(
+            [
+                (pl.col("moi_enum") == 2).alias("is_recessive"),
+                pl.when(pl.col("moi_enum") == 2)
+                .then(
+                    pl.format(
+                        "recessive|{}|{}|{}",
+                        pl.col("geneSymbol"),
+                        pl.col("score"),
+                        pl.col("moi_enum"),
                     )
                 )
-
-        return simplified_exomiser_result
-
-
-class PhEvalVariantResultFromExomiserJsonCreator:
-
-    def __init__(self, exomiser_json_result: [dict], score_name: str):
-        self.exomiser_json_result = exomiser_json_result
-        self.score_name = score_name
-
-    @staticmethod
-    def _find_chromosome(result_entry: dict) -> str:
-        """Return chromosome from Exomiser result entry."""
-        return result_entry["contigName"]
-
-    @staticmethod
-    def _find_start_pos(result_entry: dict) -> int:
-        """Return start position from Exomiser result entry."""
-        return result_entry["start"]
-
-    @staticmethod
-    def _find_end_pos(result_entry: dict) -> int:
-        """Return end position from Exomiser result entry."""
-        return result_entry["end"]
-
-    @staticmethod
-    def _find_ref(result_entry: dict) -> str:
-        """Return reference allele from Exomiser result entry."""
-        return result_entry["ref"]
-
-    @staticmethod
-    def _find_alt(result_entry: dict) -> str:
-        """Return alternate allele from Exomiser result entry."""
-        if "alt" in result_entry and result_entry["alt"] is not None:
-            return result_entry["alt"].strip(">").strip("<")
-        else:
-            return ""
-
-    def _find_relevant_score(self, result_entry) -> float:
-        """Return score from Exomiser result entry."""
-        return round(result_entry[self.score_name], 4)
-
-    def _filter_for_acmg_assignments(
-        self, variant: PhEvalVariantResult, score: float, variant_acmg_assignments: dict
-    ) -> bool:
-        """Filter variants if they meet the PATHOGENIC or LIKELY_PATHOGENIC ACMG classification."""
-        for assignment in variant_acmg_assignments:
-            if variant == PhEvalVariantResult(
-                chromosome=self._find_chromosome(assignment["variantEvaluation"]),
-                start=self._find_start_pos(assignment["variantEvaluation"]),
-                end=self._find_end_pos(assignment["variantEvaluation"]),
-                ref=self._find_ref(assignment["variantEvaluation"]),
-                alt=self._find_alt(assignment["variantEvaluation"]),
-                score=score,
-            ) and (
-                assignment["acmgClassification"] == "PATHOGENIC"
-                or assignment["acmgClassification"] == "LIKELY_PATHOGENIC"
-            ):
-                return True
-
-    def extract_pheval_variant_requirements(
-        self, use_acmg_filter: bool = False
-    ) -> [PhEvalVariantResult]:
-        """Extract data required to produce PhEval variant output."""
-        simplified_exomiser_result = []
-        for result_entry in self.exomiser_json_result:
-            for gene_hit in result_entry["geneScores"]:
-                if self.score_name in result_entry:
-                    if "contributingVariants" in gene_hit:
-                        score = self._find_relevant_score(result_entry)
-                        contributing_variants = gene_hit["contributingVariants"]
-                        variant_acmg_assignments = gene_hit["acmgAssignments"]
-                        for cv in contributing_variants:
-                            variant = PhEvalVariantResult(
-                                chromosome=self._find_chromosome(cv),
-                                start=self._find_start_pos(cv),
-                                end=self._find_end_pos(cv),
-                                ref=self._find_ref(cv),
-                                alt=self._find_alt(cv),
-                                score=score,
-                            )
-                            if use_acmg_filter and self._filter_for_acmg_assignments(
-                                variant, score, variant_acmg_assignments
-                            ):
-                                simplified_exomiser_result.append(variant)
-                            if not use_acmg_filter:
-                                simplified_exomiser_result.append(variant)
-        return simplified_exomiser_result
-
-
-class PhEvalDiseaseResultFromExomiserJsonCreator:
-    def __init__(self, exomiser_json_result: [dict]):
-        self.exomiser_json_result = exomiser_json_result
-
-    @staticmethod
-    def _find_disease_name(result_entry: dict) -> str:
-        """Return disease term from Exomiser result entry."""
-        return result_entry["diseaseTerm"]
-
-    @staticmethod
-    def _find_disease_identifier(result_entry: dict) -> int:
-        """Return disease ID from Exomiser result entry."""
-        return result_entry["diseaseId"]
-
-    @staticmethod
-    def _find_relevant_score(result_entry) -> float:
-        """Return score from Exomiser result entry."""
-        return round(result_entry["score"], 4)
-
-    def extract_pheval_disease_requirements(self) -> [PhEvalDiseaseResult]:
-        """Extract data required to produce PhEval disease output."""
-        simplified_exomiser_result = []
-        for result_entry in self.exomiser_json_result:
-            try:
-                for disease in result_entry["priorityResults"]["HIPHIVE_PRIORITY"][
-                    "diseaseMatches"
-                ]:
-                    simplified_exomiser_result.append(
-                        PhEvalDiseaseResult(
-                            disease_name=self._find_disease_name(disease["model"]),
-                            disease_identifier=self._find_disease_identifier(disease["model"]),
-                            score=self._find_relevant_score(disease),
-                        )
+                .otherwise(
+                    pl.format(
+                        "dominant|{}|{}|{}|{}|{}|{}",
+                        pl.col("chrom"),
+                        pl.col("start"),
+                        pl.col("end"),
+                        pl.col("ref"),
+                        pl.col("alt"),
+                        pl.col("score"),
                     )
-            except KeyError:
-                pass
-        return list(
-            {
-                (result.disease_identifier, result.score): result
-                for result in simplified_exomiser_result
-            }.values()
+                )
+                .alias("group_key"),
+            ]
         )
+        .with_columns(
+            [
+                pl.col("group_key")
+                .rank("dense")
+                .cast(pl.UInt32)
+                .map_elements(
+                    lambda i: str(uuid.uuid5(uuid.NAMESPACE_DNS, str(i))), return_dtype=pl.String
+                )
+                .alias("grouping_id")
+            ]
+        )
+        .select(
+            ["chrom", "start", "end", "ref", "alt", "score", "modeOfInheritance", "grouping_id"]
+        )
+    )
 
 
 def create_standardised_results(
-    results_dir: Path,
+    result_dir: Path,
     output_dir: Path,
+    phenopacket_dir: Path,
     score_name: str,
     sort_order: str,
-    variant_analysis: bool,
     gene_analysis: bool,
     disease_analysis: bool,
-    include_acmg: bool = False,
-) -> None:
-    """Write standardised gene/variant/disease results from default Exomiser json output."""
-    for exomiser_json_result in files_with_suffix(results_dir, ".json"):
-        exomiser_result = read_exomiser_json_result(exomiser_json_result)
+    variant_analysis: bool,
+):
+    sort_order = SortOrder.ASCENDING if sort_order.lower() == "ascending" else SortOrder.DESCENDING
+    for exomiser_json_result_path in files_with_suffix(result_dir, ".json"):
+        exomiser_json_result = pl.read_json(exomiser_json_result_path)
         if gene_analysis:
-            pheval_gene_requirements = PhEvalGeneResultFromExomiserJsonCreator(
-                exomiser_result, score_name
-            ).extract_pheval_gene_requirements()
-            generate_pheval_result(
-                pheval_result=pheval_gene_requirements,
-                sort_order_str=sort_order,
+            gene_results = extract_gene_results_from_json(exomiser_json_result, score_name)
+            generate_gene_result(
+                results=gene_results,
+                sort_order=sort_order,
                 output_dir=output_dir,
-                tool_result_path=trim_exomiser_result_filename(exomiser_json_result),
-            )
-        if variant_analysis:
-            pheval_variant_requirements = PhEvalVariantResultFromExomiserJsonCreator(
-                exomiser_result, score_name
-            ).extract_pheval_variant_requirements(include_acmg)
-            generate_pheval_result(
-                pheval_result=pheval_variant_requirements,
-                sort_order_str=sort_order,
-                output_dir=output_dir,
-                tool_result_path=trim_exomiser_result_filename(exomiser_json_result),
+                result_path=trim_exomiser_result_filename(exomiser_json_result_path),
+                phenopacket_dir=phenopacket_dir,
             )
         if disease_analysis:
-            pheval_disease_requirements = PhEvalDiseaseResultFromExomiserJsonCreator(
-                exomiser_result
-            ).extract_pheval_disease_requirements()
-            generate_pheval_result(
-                pheval_result=pheval_disease_requirements,
-                sort_order_str=sort_order,
+            disease_results = extract_disease_results_from_json(exomiser_json_result)
+            generate_disease_result(
+                results=disease_results,
+                sort_order=sort_order,
                 output_dir=output_dir,
-                tool_result_path=trim_exomiser_result_filename(exomiser_json_result),
+                result_path=trim_exomiser_result_filename(exomiser_json_result_path),
+                phenopacket_dir=phenopacket_dir,
+            )
+
+        if variant_analysis:
+            variant_results = extract_variant_results_from_json(exomiser_json_result, score_name)
+            generate_variant_result(
+                results=variant_results,
+                sort_order=sort_order,
+                output_dir=output_dir,
+                result_path=trim_exomiser_result_filename(exomiser_json_result_path),
+                phenopacket_dir=phenopacket_dir,
             )
 
 
@@ -250,6 +182,14 @@ def create_standardised_results(
     required=True,
     metavar="DIRECTORY",
     help="Full path to Exomiser results directory to be standardised.",
+    type=Path,
+)
+@click.option(
+    "--phenopacket-dir",
+    "-p",
+    required=True,
+    metavar="DIRECTORY",
+    help="Full path to phenopacket dir used to generate the raw results.",
     type=Path,
 )
 @click.option(
@@ -288,22 +228,15 @@ def create_standardised_results(
     default=False,
     help="Specify whether to create PhEval disease results.",
 )
-@click.option(
-    "--include-acmg",
-    is_flag=True,
-    type=bool,
-    default=False,
-    help="Specify whether to include ACMG filter for PATHOGENIC or LIKELY_PATHOGENIC classifications.",
-)
 def post_process_exomiser_results(
     output_dir: Path,
     results_dir: Path,
+    phenopacket_dir: Path,
     score_name: str,
     sort_order: str,
     gene_analysis: bool,
     variant_analysis: bool,
     disease_analysis: bool,
-    include_acmg: bool,
 ):
     """Post-process Exomiser json results into PhEval gene and variant outputs."""
     (
@@ -322,12 +255,12 @@ def post_process_exomiser_results(
         else None
     )
     create_standardised_results(
-        results_dir,
-        output_dir,
-        score_name,
-        sort_order,
-        variant_analysis,
-        gene_analysis,
-        disease_analysis,
-        include_acmg,
+        result_dir=results_dir,
+        output_dir=output_dir,
+        phenopacket_dir=phenopacket_dir,
+        score_name=score_name,
+        sort_order=sort_order,
+        variant_analysis=variant_analysis,
+        gene_analysis=gene_analysis,
+        disease_analysis=disease_analysis,
     )
